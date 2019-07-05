ec753a3f24c7f14505c2ecebe251cbaca16a32ad
Andrew Lam <andrew.c.lam94@gmail.com>
2019-07-05 00:06:10 -0400

    Refactor naming

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,89 +4,89 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
-	public InputField inputField;
+  public InputField inputField;
 
-	public Dictionary<string, Outcome> currentOutcomes = new Dictionary<string, Outcome>();
-	private Navigator navigator;
-	private HistoryLog historyLog;
+  public Dictionary<string, Outcome> currentOutcomes = new Dictionary<string, Outcome>();
+  private Navigator navigator;
+  private HistoryLog historyLog;
 
-	[SerializeField] private Outcome startingScenario;
+  [SerializeField] private Outcome startingScenario;
 
-	void Start() {
-		historyLog = GameObject.Find("HistoryLog").GetComponent<HistoryLog>();
-		inputField = GameObject.Find("InputField").GetComponent<InputField>();
-		navigator = GetComponent<Navigator>();
+  void Start() {
+    historyLog = GameObject.Find("HistoryLog").GetComponent<HistoryLog>();
+    inputField = GameObject.Find("InputField").GetComponent<InputField>();
+    navigator = GetComponent<Navigator>();
 
-		LoadScenario(startingScenario);
+    LoadScenario(startingScenario);
 
-		inputField.onEndEdit.AddListener(delegate {
-			HandleOnEditEnd(inputField.text);
-		});
-	}
+    inputField.onEndEdit.AddListener(delegate {
+      HandleOnEditEnd(inputField.text);
+    });
+  }
 
-	public void LoadScenario(Outcome scenario) {
-		historyLog.AppendText(scenario.outcomeName);
-		historyLog.AppendText(scenario.description);
-		SetCurrentOutcomes(scenario.actions);
-		inputField.Select();
-	}
+  public void LoadScenario(Outcome scenario) {
+    historyLog.AppendText(scenario.outcomeName);
+    historyLog.AppendText(scenario.description);
+    SetCurrentOutcomes(scenario.actions);
+    inputField.Select();
+  }
 
-	private void SetCurrentOutcomes(List<Action> actions) {
-		int counter = 1;
+  private void SetCurrentOutcomes(List<Action> actions) {
+    int counter = 1;
 
-		currentOutcomes.Clear();
+    currentOutcomes.Clear();
 
-		if (actions.Count > 0) {
-			actions.ForEach(action => {
-				string actionName = action.actionName.ToLower();
-				Outcome outcome = GetOutcome(action);
+    if (actions.Count > 0) {
+      actions.ForEach(action => {
+        string actionName = action.actionName.ToLower();
+        Outcome outcome = GetOutcome(action);
 
-				// Todo: Move to appropriate location
-				// UpdatePlayerStats(action);
+        // Todo: Move to appropriate location
+        // UpdatePlayerStats(action);
 
-				currentOutcomes.Add(actionName, outcome);
-				historyLog.AppendText(counter.ToString() + ". " + action.actionName);
-				counter++;
-			});
-		}
-	}
+        currentOutcomes.Add(actionName, outcome);
+        historyLog.AppendText(counter.ToString() + ". " + action.actionName);
+        counter++;
+      });
+    }
+  }
 
-	private void HandleOnEditEnd(string userInput) {
-		string lowerCaseInput = userInput.ToLower();
+  private void HandleOnEditEnd(string userInput) {
+    string lowerCaseInput = userInput.ToLower();
 
-		historyLog.AppendText(userInput);
-		inputField.text = null;
+    historyLog.AppendText(userInput);
+    inputField.text = null;
 
-		if (currentOutcomes.ContainsKey(lowerCaseInput)) {
-			LoadScenario(currentOutcomes[lowerCaseInput]);
-		} else {
-			historyLog.AppendText("Invalid input");
-			inputField.Select();
-		}
-	}
+    if (currentOutcomes.ContainsKey(lowerCaseInput)) {
+      LoadScenario(currentOutcomes[lowerCaseInput]);
+    } else {
+      historyLog.AppendText("Invalid input");
+      inputField.Select();
+    }
+  }
 
-	private Outcome GetOutcome(Action action) {
-		bool isSuccess = CalculateSuccess(action);
-		List<Outcome> outcomes = action.outcomes;
+  private Outcome GetOutcome(Action action) {
+    bool isSuccess = CalculateSuccess(action);
+    List<Outcome> outcomes = action.outcomes;
 
-		if (outcomes.Count > 0) {
-			return isSuccess ? outcomes[0] : outcomes[1];
-		}
+    if (outcomes.Count > 0) {
+      return isSuccess ? outcomes[0] : outcomes[1];
+    }
 
-		return null;
-	}
+    return null;
+  }
 
-	private bool CalculateSuccess(Action action) {
-		int diceMax = action.dice;
-		int roll = Random.Range(0, diceMax);
-		// Todo: Implement dice modifier
-		Debug.Log("Roll for action:" + action.actionName + " " + roll + " / " + diceMax);
-		bool isSuccess = roll >= (diceMax / 2);
+  private bool CalculateSuccess(Action action) {
+    int diceMax = action.dice;
+    int roll = Random.Range(0, diceMax);
+    // Todo: Implement dice modifier
+    Debug.Log("Roll for action:" + action.actionName + " " + roll + " / " + diceMax);
+    bool isSuccess = roll >= (diceMax / 2);
 
-		return isSuccess;
-	}
+    return isSuccess;
+  }
 
-	private void UpdatePlayerStats(Action action) {
-		// Todo: Implement
-	}
+  private void UpdatePlayerStats(Action action) {
+    // Todo: Implement
+  }
 }
---
+++ Assets/Scripts/ReturnWithNothing.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "DMH/Return Home With Nothing")]
+public class ReturnWithNothing : Outcome {
+	public ReturnWithNothing() {
+		outcomeName = "";
+		description = "You return home, with nothing to show for your wasted day.";
+	}
+}
