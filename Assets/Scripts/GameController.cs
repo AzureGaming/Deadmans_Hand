360730388c8cf3d49fefb1e76e857aefccc70e0e
Andrew Lam <drewclam@hotmail.com>
2019-07-14 14:16:56 -0400

    Add food and heat icons

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,90 +3,106 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameController : MonoBehaviour {
-  public InputField inputField;
+public class GameController : MonoBehaviour
+{
+    public InputField inputField;
 
-  public Dictionary<string, Outcome> currentOutcomes = new Dictionary<string, Outcome>();
-  private Navigator navigator;
-  private HistoryLog historyLog;
+    public Dictionary<string, Outcome> currentOutcomes = new Dictionary<string, Outcome>();
+    private Navigator navigator;
+    private HistoryLog historyLog;
 
-  [SerializeField] private Outcome startingScenario;
+    [SerializeField] private Outcome startingScenario;
 
-  void Start() {
-    historyLog = GameObject.Find("HistoryLog").GetComponent<HistoryLog>();
-    inputField = GameObject.Find("InputField").GetComponent<InputField>();
-    navigator = GetComponent<Navigator>();
+    void Start()
+    {
+        historyLog = GameObject.Find("HistoryLog").GetComponent<HistoryLog>();
+        inputField = GameObject.Find("InputField").GetComponent<InputField>();
+        navigator = GetComponent<Navigator>();
 
-    LoadScenario(startingScenario);
+        LoadScenario(startingScenario);
 
-    inputField.onEndEdit.AddListener(delegate {
-      HandleOnEditEnd(inputField.text);
-    });
-  }
-
-  public void LoadScenario(Outcome scenario) {
-    historyLog.AppendText(scenario.outcomeName);
-    historyLog.AppendText(scenario.description);
-    SetCurrentOutcomes(scenario.actions);
-    inputField.Select();
-  }
-
-  private void SetCurrentOutcomes(List<Action> actions) {
-    int counter = 1;
-
-    currentOutcomes.Clear();
-
-    if (actions.Count > 0) {
-      actions.ForEach(action => {
-        string actionName = action.actionName.ToLower();
-        Outcome outcome = GetOutcome(action);
-
-        // Todo: Move to appropriate location
-        // UpdatePlayerStats(action);
-
-        currentOutcomes.Add(actionName, outcome);
-        historyLog.AppendText(counter.ToString() + ". " + action.actionName);
-        counter++;
-      });
-    }
-  }
-
-  private void HandleOnEditEnd(string userInput) {
-    string lowerCaseInput = userInput.ToLower();
-
-    historyLog.AppendText(userInput);
-    inputField.text = null;
-
-    if (currentOutcomes.ContainsKey(lowerCaseInput)) {
-      LoadScenario(currentOutcomes[lowerCaseInput]);
-    } else {
-      historyLog.AppendText("Invalid input");
-      inputField.Select();
-    }
-  }
-
-  private Outcome GetOutcome(Action action) {
-    bool isSuccess = CalculateSuccess(action);
-    List<Outcome> outcomes = action.outcomes;
-
-    if (outcomes.Count > 0) {
-      return isSuccess ? outcomes[0] : outcomes[1];
+        inputField.onEndEdit.AddListener(delegate
+        {
+            HandleOnEditEnd(inputField.text);
+        });
     }
 
-    return null;
-  }
+    public void LoadScenario(Outcome scenario)
+    {
+        historyLog.ClearText();
+        historyLog.AppendText(scenario.outcomeName);
+        historyLog.AppendText(scenario.description);
+        SetCurrentOutcomes(scenario.actions);
+        inputField.Select();
+    }
 
-  private bool CalculateSuccess(Action action) {
-    int diceMax = action.dice;
-    int roll = Random.Range(0, diceMax);
-    // Todo: Implement dice modifier
-    Debug.Log("Roll for action:" + action.actionName + " " + roll + " / " + diceMax);
-    bool isSuccess = roll >= (diceMax / 2);
+    private void SetCurrentOutcomes(List<Action> actions)
+    {
+        int counter = 1;
 
-    return isSuccess;
-  }
+        currentOutcomes.Clear();
 
-  private void UpdatePlayerStats(Action action) {
-    // Todo: Implement
-  }
+        if (actions.Count > 0)
+        {
+            actions.ForEach(action =>
+            {
+                string actionName = action.actionName.ToLower();
+                Outcome outcome = GetOutcome(action);
+
+                // Todo: Move to appropriate location
+                // UpdatePlayerStats(action);
+
+                currentOutcomes.Add(actionName, outcome);
+                historyLog.AppendText(counter.ToString() + ". " + action.actionName);
+                counter++;
+            });
+        }
+    }
+
+    private void HandleOnEditEnd(string userInput)
+    {
+        string lowerCaseInput = userInput.ToLower();
+
+        historyLog.AppendText(userInput);
+        inputField.text = null;
+
+        if (currentOutcomes.ContainsKey(lowerCaseInput))
+        {
+            LoadScenario(currentOutcomes[lowerCaseInput]);
+        }
+        else
+        {
+            historyLog.AppendText("Invalid input");
+            inputField.Select();
+        }
+    }
+
+    private Outcome GetOutcome(Action action)
+    {
+        bool isSuccess = CalculateSuccess(action);
+        List<Outcome> outcomes = action.outcomes;
+
+        if (outcomes.Count > 0)
+        {
+            return isSuccess ? outcomes[0] : outcomes[1];
+        }
+
+        return null;
+    }
+
+    private bool CalculateSuccess(Action action)
+    {
+        int diceMax = action.dice;
+        int roll = Random.Range(0, diceMax);
+        // Todo: Implement dice modifier
+        Debug.Log("Roll for action:" + action.actionName + " " + roll + " / " + diceMax);
+        bool isSuccess = roll >= (diceMax / 2);
+
+        return isSuccess;
+    }
+
+    private void UpdatePlayerStats(Action action)
+    {
+        // Todo: Implement
+    }
 }
--- Assets/Scripts/HistoryLog.cs
+++ Assets/Scripts/HistoryLog.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class HistoryLog : MonoBehaviour {
-	private Text textComp;
-	void Awake() {
-		textComp = GameObject.Find("HistoryLog").GetComponent<Text>();
-		textComp.text = "";
-	}
+    private Text textComp;
+    void Awake() {
+        textComp = GameObject.Find("HistoryLog").GetComponent<Text>();
+        textComp.text = "";
+    }
 
-	public void AppendText(string text) {
-		textComp.text += text + "\n";
-	}
+    public void AppendText(string text) {
+        textComp.text += text + "\n";
+    }
+
+    public void ClearText() {
+        textComp.text = "";
+    }
 }
